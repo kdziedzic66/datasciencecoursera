ef5f9be19fc764ebbb3c269c0557742ddafde250
kdziedzic66 <kdziedzic66@gmail.com>
2015-09-08 12:35:08 +0200

    commited

--- plot1.R
+++ plot1.R
@@ -5,8 +5,9 @@ classes<-c("character","character","numeric","numeric","numeric","numeric","nume
 data<-read.csv("household_power_consumption.txt",header=TRUE,sep=";",colClasses=classes,na.strings = "?")
 data<-data%>%select(Date:Sub_metering_3)
 data<-data%>%filter(Date=="1/2/2007"|Date=="2/2/2007")
-par(mfrow=c(1,1),mar=c(4,2,2,2))
+par(mfrow=c(1,1),mar=c(4,4,2,2))
 with(data,hist(Global_active_power,xlab="Global active power (kilowatts)",main="Global Active Power",col="red",ylim=c(0,1200)))
 
 dev.copy(png,"plot1.png")
 dev.off()
+
